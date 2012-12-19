803746864654e38462fa3bf628f2d766d410c4b6
Guilherme Pereira Dutra <guipereira07@gmail.com>
2012-12-19 17:41:37 -0200

    Update spec/roman_numerals_spec.rb

--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 describe "Roman Numerals" do
   it "should convert roman number to literal number given a basic roman number" do
     roman_number = RomanNumeral.new("X")
-    roman_number.to_literal.should eql 1
+    roman_number.to_literal.should eql 10
   end
   it "should convert roman number to literal number given a complex roman number" do  
     roman_number = RomanNumeral.new("MCMXCIX")
